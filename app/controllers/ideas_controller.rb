1d61b418779152d2987c9f747f1d02715a53fb50
Sandeep <sandeeppoudel437@gmail.com>
2016-08-29 15:10:59 +0000

    Editing and Updating ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,11 +3,24 @@ class IdeasController < ApplicationController
         # this should be @ideas not @idea :)
         @ideas = Idea.all
     end
+    
     def create
-        @ideas = Idea.create(idea_params)
+        @idea = Idea.create(idea_params)
         redirect_to root_path
     end
     
+    def edit
+        @ideas = Idea.find(params[:id])
+    end
+    
+    def update
+         @ideas = Idea.create(idea_params)
+         if @idea.update(idea_params)
+             redirect_to root_path
+        else
+            redirect_to edit_idea_path(params[:id])
+        end
+    end
     private
     def idea_params
         params.require(:idea).permit(:description, :author)
